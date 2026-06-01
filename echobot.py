86e3d5600092b8d4fd6f3389ba6855e52e229a6d
djumanov <djumanovdev@gmail.com>
2026-06-01 18:08:18 +0500

    echo bot

--- echobot.py
+++ echobot.py
@@ -22,6 +22,12 @@ def send_message(chat_id: int, text: str):
     requests.get(url, params=params)
 
 
+def send_photo(chat_id: int, photo: str):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
+    params = {"chat_id": chat_id, "photo": photo}
+    requests.get(url, params=params)
+
+
 def main():
     update_id = None
 
@@ -32,6 +38,11 @@ def main():
                     send_message(
                         chat_id=update["message"]["chat"]["id"],
                         text=update["message"]["text"],
+                    )
+                elif update['message'].get('photo'):
+                    send_photo(
+                        chat_id=update["message"]["chat"]["id"],
+                        photo=update["message"]["photo"][0]['file_id'],
                     )
 
             update_id = update["update_id"] + 1
